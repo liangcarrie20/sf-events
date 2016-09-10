8665ec13a1739fd29d5c08e44145206fee7361a6
Maria Uyeda <mariauyeda1@gmail.com>
2016-09-10 12:44:52 -0700

    Add event model and seed database with fake events.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,13 @@
 User.create(email: 'bobbysags@gmail.com', name: 'Bob Saget', password: '123')
+
+
+20.times do 
+	Event.create(
+		title: Faker::Company.bs,
+		description: Faker::Lorem.sentences(3),
+		address: Faker::Address.street_address,
+		event_date: Faker::Date.forward(23),
+		event_time: Faker::Time.backward(14, :evening),
+		price: Faker::Commerce.price
+		)
+end
